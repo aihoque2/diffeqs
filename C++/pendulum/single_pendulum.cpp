18474303c11c3020f8b709e7d873b7ddb30e2c56
aihoque2 <aihoque2@illinois.edu>
2021-10-30 02:46:09 -0500

    stuck with using static or not for the constants

--- C++/pendulum/single_pendulum.cpp
+++ C++/pendulum/single_pendulum.cpp
@@ -9,21 +9,33 @@ using namespace std;
 /*
 let's spin a simple pendulum around 
 this is taking 3Blue1Brown's implementation, but in C++!
+mu for dampening constant
 */
 
 class Pendulum{
+    //solve a classical mechanics pendulum problem with euler integration
     public:
         Pendulum(); //constructor for our solver
-        double g; //gravity. are we on Earth, Mars, Venus, Or Pluto?
-        double L; //length of pendulum
+        Pendulum(double gravity, double length, double air_resistance)
+        static const double g; //gravity. are we on Earth, Mars, Venus, Or Pluto?
+        static const double L; //length of pendulum
+        static const double mu; //dampening constant
+        double get_theta_double_dot(double theta, double theta_dot);
         void run(double dt, double t0, double end_time);
         
 };
 
 Pendulum::Pendulum(){
-    g = 9.8;
-    L = 12.0;
-        
+    // some default constructor
+    static const double g = 9.8;
+    static const double L = 12.0;
+    static const double mu = 0.1;
 }
+
+Pendulum::Pendulum(double gravity, double length, double air_resistance){
+    static const double g = gravity;
+    static const double 
+}
+
 const double g = 9.8;
 const double L = 12.0; //length of the pendulum 
