ffd89ca3bbdf4f2f91c09a1252f11cc77f48cbce
Ammar Hoque <aihoque2@illinois.edu>
2022-01-15 23:27:37 -0600

    added header fn's

--- C++/richards_eqn/richards_explicit_fd.hpp
+++ C++/richards_eqn/richards_explicit_fd.hpp
@@ -6,3 +6,13 @@
 # include <cmath>
 
 using namespace std;
+
+double *fd1d_richards_explicit( int x_num, double x[], double t, double dt, 
+  double cfl, double *rhs ( int x_num, double x[], double t ), 
+  void bc ( int x_num, double x[], double t, double h[] ), double h[] );
+double fd1d_richards_explicit_cfl( double k, int t_num, double t_min, double t_max, 
+  int x_num, double x_min, double x_max );
+void r8mat_write ( string output_filename, int m, int n, double table[] );
+double *r8vec_linspace_new ( int n, double a_first, double a_last );
+void r8vec_write ( string output_filename, int n, double x[] );
+void timestamp ( );
